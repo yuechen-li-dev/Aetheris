0b50a6c02bc616ae9905b3c24d391291259d4f0d
Yuechen Li <109704248+yuechen-li-dev@users.noreply.github.com>
2026-02-26 03:13:20 -0800

    Harden M23 importer determinism and diagnostics

--- Aetheris.Kernel.Core.Tests/Step242/Step242ImporterTests.cs
+++ Aetheris.Kernel.Core.Tests/Step242/Step242ImporterTests.cs
@@ -58,6 +58,66 @@ public sealed class Step242ImporterTests
     }
 
     [Fact]
+    public void ImportBody_EdgeCurveSameSenseFalse_ReturnsNotImplementedDiagnostic()
+    {
+        var stepText = """
+ISO-10303-21;
+HEADER;
+ENDSEC;
+DATA;
+#1=CARTESIAN_POINT($,(0,0,0));
+#2=VERTEX_POINT($,#1);
+#3=CARTESIAN_POINT($,(1,0,0));
+#4=VERTEX_POINT($,#3);
+#5=CARTESIAN_POINT($,(0,0,0));
+#6=DIRECTION($,(1,0,0));
+#7=VECTOR($,#6,1);
+#8=LINE($,#5,#7);
+#9=EDGE_CURVE($,#2,#4,#8,.F.);
+#10=ORIENTED_EDGE($,$,$,#9,.T.);
+#11=EDGE_LOOP($,(#10));
+#12=FACE_OUTER_BOUND($,#11,.T.);
+#13=CARTESIAN_POINT($,(0,0,0));
+#14=DIRECTION($,(0,0,1));
+#15=DIRECTION($,(1,0,0));
+#16=AXIS2_PLACEMENT_3D($,#13,#14,#15);
+#17=PLANE($,#16);
+#18=ADVANCED_FACE((#12),#17,.T.);
+#19=CLOSED_SHELL($,(#18));
+#20=MANIFOLD_SOLID_BREP('x',#19);
+ENDSEC;
+END-ISO-10303-21;
+""";
+
+        var import = Step242Importer.ImportBody(stepText);
+
+        Assert.False(import.IsSuccess);
+        var diagnostic = Assert.Single(import.Diagnostics);
+        Assert.Equal(KernelDiagnosticCode.NotImplemented, diagnostic.Code);
+        Assert.Equal("Entity:9", diagnostic.Source);
+        Assert.Contains("same_sense=.F.", diagnostic.Message, StringComparison.Ordinal);
+    }
+
+    [Fact]
+    public void ImportBody_MultipleBrepRoots_ReturnsDeterministicDiagnostic()
+    {
+        var fixture = CreateM22BoxFixtureText();
+        var duplicateRoot = fixture.Replace(
+            "ENDSEC;\nEND-ISO-10303-21;",
+            "#999=MANIFOLD_SOLID_BREP('other',#19);\nENDSEC;\nEND-ISO-10303-21;",
+            StringComparison.Ordinal);
+
+        var import = Step242Importer.ImportBody(duplicateRoot);
+
+        Assert.False(import.IsSuccess);
+        var diagnostic = Assert.Single(import.Diagnostics);
+        Assert.Equal(KernelDiagnosticCode.NotImplemented, diagnostic.Code);
+        Assert.Equal("Importer", diagnostic.Source);
+        Assert.Contains("Multiple MANIFOLD_SOLID_BREP", diagnostic.Message, StringComparison.Ordinal);
+        Assert.Contains("#999", diagnostic.Message, StringComparison.Ordinal);
+    }
+
+    [Fact]
     public void ImportBody_UnsupportedEntityInParseableText_ReturnsNotImplementedDiagnostic()
     {
         var unsupported = "ISO-10303-21;\nHEADER;\nENDSEC;\nDATA;\n#1=SPHERICAL_SURFACE($,#2,1.0);\n#2=AXIS2_PLACEMENT_3D($,#3,#4,#5);\n#3=CARTESIAN_POINT($,(0,0,0));\n#4=DIRECTION($,(0,0,1));\n#5=DIRECTION($,(1,0,0));\nENDSEC;\nEND-ISO-10303-21;";

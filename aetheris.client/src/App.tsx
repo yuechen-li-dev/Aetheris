b5877e585ee8f8545b37f4846750c919273583c6
Yuechen Li <109704248+yuechen-li-dev@users.noreply.github.com>
2026-03-07 03:44:53 -0800

    Refine header wordmark hierarchy and alignment

--- aetheris.client/src/App.tsx
+++ aetheris.client/src/App.tsx
@@ -499,7 +499,11 @@ function App() {
                 <div className="top-bar__header-row">
                     <div className="top-bar__wordmark" aria-label="AETHERIS CADMATA">
                         <span className="top-bar__wordmark-primary">AETHERIS</span>
-                        <span className="top-bar__wordmark-secondary">CADMATA</span>
+                        <span className="top-bar__wordmark-secondary" aria-hidden="true">
+                            {'CADMATA'.split('').map((character, index) => (
+                                <span key={`${character}-${index}`}>{character}</span>
+                            ))}
+                        </span>
                     </div>
                     <div className="top-bar__actions-block">
                         <div className="top-bar__actions">
